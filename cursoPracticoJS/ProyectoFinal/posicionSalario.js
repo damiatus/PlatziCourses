f934a516fb55b269593d13cd9a1566a259d364f1
damiatus <damiandeaguiar@gmail.com>
2022-02-03 15:47:48 -0300

    Creo el archivo datosPaises donde estará la base de datos de mis países, esto queda referenciado en el archivo posicionSalario quedando listo para usar

--- cursoPracticoJS/ProyectoFinal/posicionSalario.js
+++ cursoPracticoJS/ProyectoFinal/posicionSalario.js
@@ -1,6 +1,20 @@
+let salariosChile = [];
 
-const inputSalary = document.getElementById("InputSalary");
-const inputPais = document.getElementById("InputCountry");
+//Helpers
+chile.forEach(function(persona){
+    salariosChile.push(persona.salary);
+    });
+
+const salariosChileSorted = salariosChile.sort(
+    function(salaryA, salaryB){
+        return salaryA - salaryB;
+    }
+);
+
+//Funcion principal
+
+const inputSalary = document.getElementById("InputSalary").value;
+const inputPais = document.getElementById("InputCountry").value;
 
 
 function calcularPos(){
@@ -10,3 +24,4 @@ function calcularPos(){
 
     resultPos.innerText = "Tu posicion en Chile? Latama? es con descuento es top " + posicionPais + "%";
 };
+
